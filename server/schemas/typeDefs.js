d4003936557c84779bde333542094ef304112a92
anrichter <anrichter2@gmail.com>
2024-10-01 20:25:28 -0500

    Changed the typeDefs to include our type of models and added basic queries and mutations.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,25 +1,41 @@
-const typeDefs = `
+const typeDefs = `#graphql
   type User {
     _id: ID
     username: String
     email: String
     password: String
-    thoughts: [Thought]!
+    weight: String
+    height: String
+    workouts: [Fitness]!
+    foodIntake: [Nutrition]!
   }
 
-  type Thought {
+  type Fitness {
     _id: ID
-    thoughtText: String
-    thoughtAuthor: String
-    createdAt: String
-    comments: [Comment]!
+    exerciseDate: Date
+    totalCalBurn: Int
+    exercises: [Exercise]!
   }
 
-  type Comment {
+  type Exercise {
     _id: ID
-    commentText: String
-    commentAuthor: String
-    createdAt: String
+    # exerciseName: String
+    exerciseType: String
+    exerciseDuration: Int
+    caloriesBurned: Int
+  }
+
+  type Nutrition {
+    _id: ID
+    intakeDate: Date
+    totalCalIntake: Int
+    foods: [Food]
+  }
+
+  type Food {
+    _id: ID
+    foodName: String
+    calories: Int
   }
 
   type Auth {
@@ -28,20 +44,20 @@ const typeDefs = `
   }
 
   type Query {
-    users: [User]
-    user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(thoughtId: ID!): Thought
     me: User
+    fitness(fitnessId: String!): Fitness
+    nutrition(nutritionId: String!): Nutrition
   }
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    addThought(thoughtText: String!): Thought
-    addComment(thoughtId: ID!, commentText: String!): Thought
-    removeThought(thoughtId: ID!): Thought
-    removeComment(thoughtId: ID!, commentId: ID!): Thought
+    addFitness(exerciseName: String!, exerciseType: String!, exerciseDuration: Int!, caloriesBurned: Int!): Fitness
+    addNutrition(foodName: String!, calories: Int!): Nutrition
+    addExercise(fitnessId: ID!, exerciseName: String!, exerciseType: String!, exerciseDuration: Int!, caloriesBurned: Int!): Fitness
+    addFood(nutritionId: ID!, foodName: String!, calories: Int!): Nutrition
+    removeExercise(fitnessId: ID!, exerciseID: ID!): Fitness
+    removeFood(nutritionId: ID!, foodId: ID!): Nutrition
   }
 `;
 
