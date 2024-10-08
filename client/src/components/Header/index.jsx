6cf2a2975f52c988d43db346e9f0cfcd4ffebf7c
bryan-mcintyre <167257103+bryan-mcintyre@users.noreply.github.com>
2024-10-07 21:42:17 -0500

    Updated Header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -12,14 +12,14 @@ const Header = () => {
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/">
-            <h1 className="m-0">Tech Thoughts</h1>
+            <h1 className="m-0">HealthyMe</h1>
           </Link>
-          <p className="m-0">Get into the mind of a programmer.</p>
+          <p className="m-0">Build a better version of you.</p>
         </div>
         <div>
           {Auth.loggedIn() ? (
             <>
-              <Link className="btn btn-lg btn-info m-2" to="/me">
+              <Link className="btn btn-lg btn-info m-2" to="/profile">
                 {/* Run the getProfile() method to get access to the unencrypted token value in order to retrieve the user's username  */}
                 {Auth.getProfile().authenticatedPerson.username}'s profile
               </Link>
